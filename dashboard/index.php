24b555fc3179c4730f558e1f105432d7cea041f1
queencantiqabys <queenunslngii@gmail.com>
2023-04-06 04:48:14 +0700

    menambahkan result page

--- dashboard/index.php
+++ dashboard/index.php
@@ -154,15 +154,6 @@ include "../controller/controller.php";
           </a>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link text-white  <?= $btn3; ?>" href="<?= $btn3_get; ?>">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">table_view</i>
-            </div>
-            <span class="nav-link-text ms-1"><?= $btn3_name; ?></span>
-          </a>
-        </li>
-
 
         <li class="nav-item">
           <a class="nav-link text-white <?= $btn2; ?>" href="<?= $btn2_get; ?>">
@@ -173,6 +164,15 @@ include "../controller/controller.php";
           </a>
         </li>
 
+
+        <li class="nav-item">
+          <a class="nav-link text-white  <?= $btn3; ?>" href="<?= $btn3_get; ?>">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">table_view</i>
+            </div>
+            <span class="nav-link-text ms-1"><?= $btn3_name; ?></span>
+          </a>
+        </li>
 
 
         <li class="nav-item mt-3">
