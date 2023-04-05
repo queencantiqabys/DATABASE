7ebdf7240e49301dd674ab8eb96f63e677cabe6e
queencantiqabys <queenunslngii@gmail.com>
2023-04-06 06:21:08 +0700

    menampilkan hasi ujian

--- dashboard/content/result.php
+++ dashboard/content/result.php
@@ -1,3 +1,8 @@
+<?php
+    $result=select("select * from result where user_id=$userId");
+    
+?>
+
 <div class="row">
         <div class="col-12">
           <div class="card my-4">
@@ -11,40 +16,47 @@
                 <table class="table align-items-center justify-content-center mb-0">
                   <thead>
                     <tr>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Event Name</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Register Date</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Event Name</th>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Grade</th>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">Score</th>
                       <th></th>
                     </tr>
                   </thead>
                   <tbody>
-                    
 
 
+                  <?php
+                  
+                    foreach($result as $row) :
+                        $eventid=$row['event_id'];
+                        $events=mysqli_query($conn,"select event_name from events where event_id =$eventid ");
+                        $event=mysqli_fetch_assoc($events);
+                        
+                        
+                  ?>
                     <tr>
                       <td>
                         <div class="d-flex px-2">
                           <div>
-                            <img src="../assets/img/small-logos/github.svg" class="avatar avatar-sm rounded-circle me-2" alt="invision">
                           </div>
                           <div class="my-auto">
-                            <h6 class="mb-0 text-sm">Wardana</h6>
+                            <h6 class="mb-0 text-sm"><?= $data['user_name']; ?></h6>
                           </div>
                         </div>
                       </td>
                       <td>
-                        <p class="text-sm font-weight-bold mb-0">14 Juli 2003</p>
+                        <p class="text-sm font-weight-bold mb-0"><?= $event['event_name']; ?></p>
                       </td>
                       <td>
-                        <span class="text-xs font-weight-bold">A+</span>
+                        <span class="text-xs font-weight-bold"><?= $row['grade']; ?></span>
                       </td>
                       <td class="align-middle text-center">
                         <div class="d-flex align-items-center justify-content-center">
-                          <span class="me-2 text-xs font-weight-bold">100%</span>
+                          <span class="me-2 text-xs font-weight-bold"><?= $row['score']?>%</span>
                           <div>
                             <div class="progress">
-                              <div class="progress-bar bg-gradient-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>
+                              <div class="progress-bar bg-gradient-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: <?= $row['score']?>%;"></div>
                             </div>
                           </div>
                         </div>
@@ -55,6 +67,9 @@
                         </button>
                       </td>
                     </tr>
+                    <?php
+                        endforeach;
+                    ?>
                     
                     
                     
