b88a2cde093bcb999e292d1a899a4d13f087b6e9
queencantiqabys <queenunslngii@gmail.com>
2023-04-07 20:25:38 +0700

    dokumentasi create table database

--- backup.sql
+++ backup.sql
@@ -8,3 +8,56 @@ CREATE TABLE users (
 );
 
 
+CREATE TABLE admin (
+  admin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  admin_name VARCHAR(50) NOT NULL,
+  admin_num INT NOT NULL,
+  admin_email VARCHAR(100) NOT NULL,
+  admin_password VARCHAR(255) NOT NULL
+);
+
+
+
+CREATE TABLE events (
+  event_id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+  event_name VARCHAR(100) NOT NULL,
+  event_description VARCHAR(255) NOT NULL,
+  event_date DATE NOT NULL,
+  event_img VARCHAR(255) NULL,
+  waktu_mulai TIME NOT NULL,
+  waktu_selesai TIME NOT NULL
+);
+-- alter table events add column event_img varchar(255) after waktu_mulai;
+
+CREATE TABLE register (
+  register_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  register_acc BOOLEAN NOT NULL,
+  register_date DATE NOT NULL,
+  user_id INT NOT NULL,
+  event_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (event_id) REFERENCES events(event_id)
+);
+
+CREATE TABLE questions (
+question_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+event_id INT NOT NULL,
+question TEXT NOT NULL,
+question_answer CHAR(1) NOT NULL,
+a VARCHAR(255) NULL,
+b VARCHAR(255) NULL,
+c VARCHAR(255) NULL,
+d VARCHAR(255) NULL,
+e VARCHAR(255) NULL,
+FOREIGN KEY (event_id) REFERENCES events(event_id)
+);
+
+CREATE TABLE result (
+result_id INT AUTO_INCREMENT PRIMARY KEY,
+score DECIMAL(5,2),
+grade CHAR(1),
+user_id INT,
+event_id INT,
+FOREIGN KEY (user_id) REFERENCES users(user_id),
+FOREIGN KEY (event_id) REFERENCES events(event_id)
+);
